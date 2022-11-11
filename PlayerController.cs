1e900264e36778e16dd40e7322f7df0df49fde41
ARZ <72397731+aryanrekhi@users.noreply.github.com>
2022-11-11 16:07:20 +0530

    Create PlayerController.cs

---
+++ PlayerController.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class handles the movement of the player with given input from the input manager
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    [Header("Settings")]
+    //[ToolTip("Speed at which the player moves")]
+    public float moveSpeed = 2f;
+    //[ToolTip("Speed at which the player rotates to look left and right (calculated in degrees)")]
+    public float lookSpeed = 60f;
+    //[ToolTip("The power with which the player jumps")]
+    public float jumpPower = 8f;
+    //[ToolTip("The strength of gravity")]
+    public float gravity  = 9.81f;
+
+    [Header("Jump Timing")]
+    public float jumpTimeLeniency = 0.1f;
+    float timeToStopBeingLenient = 0;
+
+    [Header("Required References")]
+    //[ToolTip("The player shooter script that fires projectiles")]
+    public Shooter playerShooter;
+    public Health playerHealth;
+    public List<GameObject> disableWhileDead;
+    bool doubleJumpAvailable = false;
+
+    //Character_Controller component of the player
+    private CharacterController controller;
+    private InputManager inputManager;
+
+    /// <summary>
+    /// Description:
+    /// Standard Unity function called once before the first Update call
+    /// Input:
+    /// none
+    /// Return:
+    /// void (no return)
+    /// </summary>
+    void Start()
+    {
+        SetUpCharacterController();
+        SetUpInputManager();
+    }
+
+    private void SetUpCharacterController() 
+    {
+        controller = GetComponent<CharacterController>();
+        if (controller == null) 
+        {
+            Debug.LogError("The player controller script does not have a character controller on the same object");
+        }
+    }
+    
+    void SetUpInputManager()
+    {
+        inputManager = InputManager.instance;
+    }
+
+    /// <summary>
+    /// Description:
+    /// Standard Unity function called once every frame
+    /// Input:
+    /// none
+    /// Return:
+    /// void (no return)
+    /// </summary>
+    void Update()
+    {
+        if(playerHealth.currentHealth <= 0) 
+        {
+            foreach (GameObject inGameObject in disableWhileDead) 
+            {
+                inGameObject.SetActive(false);
+            }
+            return;
+        }
+        else 
+        {
+            foreach(GameObject inGameObject in disableWhileDead)
+            {
+                inGameObject.SetActive(true);
+            }
+        }
+
+        ProcessMovement();
+        ProcessRotation();
+    }
+    Vector3 moveDirection;
+
+    void ProcessMovement()
+    {
+        //get the imput from the input manager
+        float leftRightInput = inputManager.horizontalMoveAxis;
+        //Debug.Log("the left right input value is:" + leftRightInput);
+        float forwardBackwardInput = inputManager.verticalMoveAxis;
+        bool jumpPressed = inputManager.jumpPressed;
+
+
+        //Handle the control of the player while it is on the ground
+        if(controller.isGrounded)
+        {
+            doubleJumpAvailable = true;
+            timeToStopBeingLenient = Time.time + jumpTimeLeniency;
+            //Set the movement direction to be received input , set y to 0 since we are on ground 
+            moveDirection = new Vector3(leftRightInput, 0, forwardBackwardInput);
+            //Set the move direction in relation to the transform 
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * moveSpeed;
+
+            if (jumpPressed) 
+            {
+                moveDirection.y = jumpPower;
+            }
+        }
+        else 
+        {
+            moveDirection = new Vector3(leftRightInput * moveSpeed, moveDirection.y, forwardBackwardInput * moveSpeed);
+            moveDirection = transform.TransformDirection(moveDirection);
+            //now our player will be able to move while in the air
+
+            if(jumpPressed && Time.time < timeToStopBeingLenient)
+            {
+                moveDirection.y = jumpPower;
+            }
+            else if(jumpPressed && doubleJumpAvailable) 
+            {
+                moveDirection.y = jumpPower;
+                doubleJumpAvailable = false;
+            }
+
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        if (controller.isGrounded && moveDirection.y < 0) 
+        {
+            moveDirection.y = -0.3f;
+            //this prevents gravity from stacking up on the player while they are grounded
+            //so that whem a player walks up a ledge they do not move at an unreasonable falling speed
+
+        }
+
+        controller.Move(moveDirection*Time.deltaTime);
+    }
+
+    void ProcessRotation() 
+    {
+        float horizonatlLookInput = inputManager.horizontalLookAxis;
+        Vector3 playerRotation = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(new Vector3(playerRotation.x, playerRotation.y + horizonatlLookInput * lookSpeed *
+            Time.deltaTime, playerRotation.z));
+
+    }
+}
